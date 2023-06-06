a0784b404f9da4aafd43ab03fe75520a6b88a306
brucesensei <nolan_76@msn.com>
2023-06-06 18:55:02 +0900

    added some unknown files

--- main.py
+++ main.py
@@ -4,22 +4,43 @@ import pyperclip as pc
 
 # to run in interactive mode type python -i <file-name>
 
-listinfo = pc.paste()
+def get_pasted_info():
+    user_move = input("copy data to the clipboard, press any key to continue")
+    listinfo = pc.paste()
 
-with io.open("foe.txt","w",encoding="utf8") as f:
-    f.write(listinfo)
+    with io.open("lesson-data.json","w",encoding="utf8") as f:
+        f.write(listinfo)
 
-with io.open("foe.txt","r",encoding="utf8") as f:
-    data = f.read()
-data = data.splitlines()
+    with io.open("lesson-data.json","r",encoding="utf8") as f:
+        data = f.read()
+    data = data.splitlines()
+    return data 
 
+def create_dict(vocab):
+    i = 0
+    new_dict = {}
+    while i < len(vocab):
+        new_dict[vocab[i]] = vocab[i + 1]
+        i+=2
+    return new_dict     
 
-newdata = []
-
-for i in data:
-    if i != "":
-        newdata.append(i)
-new_data = [i.strip() for i in newdata]
-
-print(new_data)
+def get_data(file):
+    with io.open(file, "r", encoding="utf8") as f:
+        data = f.read()
+        data = json.loads(data)
+        return data
+    
+def main():
+    while True:
+        user_data = get_pasted_info()
+        lesson_info = create_dict(user_data)
+        open_data = get_data("lesson-data.json")
+        lesson_titles = [i.lower() for i in open_data]
+        while True:
+            title = input("enter the title of the lesson ").lower
+            if title in lesson_titles:
+                print(f"the lesson is already there choose another title ")
+                continue
+            break
+        
 
